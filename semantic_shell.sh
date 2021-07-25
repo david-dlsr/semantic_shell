6d063cff27513ebac3c41eb80997599aee49cc19
usergit <heicthor@gmail.com>
2021-07-25 19:53:00 -0300

    style(ProjetoGitGTV3): Foi realizada formatação do codigo para uma melhor identação. - Arq: semantic_shell.sh - In: main

--- semantic_shell.sh
+++ semantic_shell.sh
@@ -5,33 +5,38 @@
 #####################
 #  Função: Ajudar na normatização do histórico de commits em um projeto, fazendo utilização dos commits semanticos
 #  Fonte :  https://www.conventionalcommits.org/pt-br/v1.0.0-beta.4/
+#  lembrando que ainda temos as tags semânticas que ficaram para um proximo release
+#  script criado em shellscript utilizando recursos do shell
 
 
-#####################
-# VARIAVEIS GLOBAIS #
-#####################
-branch_atual="$(git b|grep ^*|awk '{print $2}')";
+#######################
+# VARIAVEIS GLOBAIS  #
+#######################
+branch_atual="$(git branch|grep ^*|awk '{print $2}')";
 msg_tag_1="Melhor pratica é manipular um arquivo por commit com essa TAG";
 msg_tag_N="Podem ser utilizados varios arquivos por commit com essa TAG";
 resp_commit_path="";
 Git="/usr/bin/git";
-#####################
-# FUNCOES DO SCRIPT #
-#####################
+# LEMBRANDO 
+# dirname  - mostra somente ate o ultimo diretorio em um path
+# basename - mostra somente o nome de um arquivo em um path
+#echo -e "Informações do script\n Nome Script: $(basename $0)\nPath Script: $(dirname ${BASH_SOURCE[0]})";
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";
+
+
+########################
+#  FUNCOES DO SCRIPT   #
+########################
 function config_proj(){
-
-# Informar diretorio do projeto [S|s|Y|y]
+# Perguntando sobre novo path e testando caso utilize, se não utilizar usa o diretorio atual de execução do script
 read -p "Iformar diretorio do commit [S|N]: " resp_commit_path;
-
-
 case $msg_commit_path in
-s|S|y|Y)  read -p "Entre com o caminho do projeto" msg_commit_path; 
-              if [ -d $msg_commit ] ;then echo "diretorio existe ! continuando ....";cd $msg_commit ;else echo "O diretorio nao existe...Saindao";fi;;
-*) echo "Sera usado o path atual: $PWD";;
-esac
+     s|S|y|Y)  read -p "Entre com o caminho do projeto" msg_commit_path; 
+          if [ -d $msg_commit ] ;then echo "diretorio existe ! continuando ....";cd $msg_commit ;else echo "O diretorio nao existe...Saindao";fi;;
+     *) echo "Sera usado o path atual: $DIR";;
+esac;
 
-# configurando principais atalhos para o git no projeto
-# git normal 
+# configurando atalhos para o git, estes seram utilizados dentro do proprio script
 $Git config --global alias.b "branch";
 $Git config --global alias.ck "checkout";
 $Git config --global alias.st "status -s";
@@ -40,7 +45,7 @@ $Git config --global alias.cm "commit -m";
 $Git config --global alias.cam "commit -a -m";
 
 # git commits semanticos
-$Git config --global alias.cs "";
+$Git config --global alias.cs "/bin/bash $PWD";
 
 # logs interessantes
 $Git config --global alias.l   "log --pretty=format:'%C(blue)%h -%<(12,trunc)%C(red)%cr - %<(11,trunc)%C(cyan)%cn - %C(white)%s - %C(yellow)%d'";
@@ -49,14 +54,10 @@ $Git config --global alias.lff "log --pretty=format:'%C(blue)%h - %<(18,trunc)%C
 
 # git editor padrao vim
 $Git config --global  core.editor "vim";
-
 }
 
-
-
-function seleciona_tag()
-{
-git status     
+function seleciona_tag(){
+git status ;    
 clear;
 echo -e "OLa DEV, Lembre de adicionar os arquivos para a staged area antes de chamar esse script !!!\nEscolha enre as Tags disponiveis para commit:\n\n\
 \033[1;36m1)\033[1;31m feat:\033[0;33m     Utilizado quando se adiciona alguma nova funcionalidade do zero ao código/serviço/projeto\n\
@@ -70,96 +71,78 @@ echo -e "OLa DEV, Lembre de adicionar os arquivos para a staged area antes de ch
 \033[1;36m9)\033[1;31m build:\033[0;33m    Usado quando se realiza alguma modificação em arquivos de build e dependências\033[0;37m\n"
 read -p "Informe o numero que se enquadra no seu commit: " resp_com;
 case $resp_com in 
-1) Tag="feat";arq_msg="$msg_tag_1";;
-2) Tag="fixH";arq_msg="$msg_tag_1";;
-3) Tag="fix";arq_msg="$msg_tag_1";;
-4) Tag="refactor";arq_msg="$msg_tag_N";;
-5) Tag="style";arq_msg="$msg_tag_N";;
-6) Tag="test";arq_msg="$msg_tag_N";;
-7) Tag="doc";arq_msg="$msg_tag_N";;
-8) Tag="env";arq_msg="$msg_tag_N";;
-9) Tag="build";arq_msg="$msg_tag_N";;
-esac
-read -p "Informe o escopo do commit (enter para vazio): "  resp_escope
+     1) Tag="feat";arq_msg="$msg_tag_1";;
+     2) Tag="fixH";arq_msg="$msg_tag_1";;
+     3) Tag="fix";arq_msg="$msg_tag_1";;
+     4) Tag="refactor";arq_msg="$msg_tag_N";;
+     5) Tag="style";arq_msg="$msg_tag_N";;
+     6) Tag="test";arq_msg="$msg_tag_N";;
+     7) Tag="doc";arq_msg="$msg_tag_N";;
+     8) Tag="env";arq_msg="$msg_tag_N";;
+     9) Tag="build";arq_msg="$msg_tag_N";;
+esac;
+read -p "Informe o escopo do commit (enter para vazio): "  resp_escope;
 export TAG escope;
-
-
 }
 
-function arq_alterado()
-{
-
+function arq_alterado(){
 cont=0;
-
-    if [ -z "$(git st|awk '{print $2}')" ]
-       then 
-            echo "Nao foi encontrado arquivo alterado na arvore...";
-            
+if [ -z "$(git st|awk '{print $2}')" ]
+     then 
+          echo "Nao foi encontrado arquivo alterado na arvore...";       
        else
-           while read entrada;
+          while read entrada;
                do   
-                echo -e  "${cont}) ${entrada}\n";
-                arquiv[$cont]=$entrada;
-                let cont++; 
-                done< <(git st|awk '{print $2}')
+                     echo -e  "${cont}) ${entrada}\n";
+                     arquiv[$cont]=$entrada;
+                     let cont++; 
+                     done< <(git st|awk '{print $2}')
                 read -p "Selecione o numero do arquivo mais relevante  para mostrar no commit:  " resp_arq_alt;
                 arquivo=${arquiv[$resp_arq_alt]};
                 export arquivo;
-    fi            
+fi;            
 }
 
-function msg_commit()
-{
-   
-   read -p "Entre com a mensagem do commit: " msg_final;
-   export msg_final;
-
-
+function msg_commit(){
+     read -p "Entre com a mensagem do commit: " msg_final;
+     export msg_final;
 }
 
-function infos()
-{
-
-# LEMBRANDO 
-# dirname  - mostra somente ate o ultimo diretorio em um path
-# basename - mostra somente o nome de um arquivo em um path
-#echo -e "Informações do script\n Nome Script: $(basename $0)\nPath Script: $(dirname ${BASH_SOURCE[0]})";
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo "mostrando local do script $(basename $0)  - $DIR"
-sleep 3
-
+function infos(){
+     echo "mostrando local do script $(basename $0)  - $DIR";
+     sleep 3;
 }
 
-#####################
-#  CORPO DO SCRIPT  #
-#####################
-
-
+#######################
+#    CORPO DO SCRIPT   #
+#######################
+# chamadas de funcoes
 infos;
 config_proj;
 seleciona_tag;
 
-
-
+# tratamento de escopo no commit
 if [ -z $escope ]
-  then 
-     
-     echo "$arq_msg $Tag";
-     arq_alterado;
-     msg_commit;
-     echo "$Tag($resp_escope): $msg_final - Arq: $arquivo - In: $branch_atual"    
-     MSG="$Tag($resp_escope): $msg_final - Arq: $arquivo - In: $branch_atual"; 
-   else
-     echo "$arq_msg $Tag";
-     arq_alterado;
-     msg_commit;
-     echo "$Tag: $msg_final - Arq: $arquivo - In: $branch_atual"
-     MSG="$Tag: $msg_final - Arq: $arquivo - In: $branch_atual";
-fi     
+     then  
+          echo "$arq_msg $Tag";
+          # chamadas de funcoes
+          arq_alterado;
+          msg_commit;
+          echo "$Tag($resp_escope): $msg_final - Arq: $arquivo - In: $branch_atual"    
+          MSG="$Tag($resp_escope): $msg_final - Arq: $arquivo - In: $branch_atual"; 
+     else
+          echo "$arq_msg $Tag";
+          # chamadas de funcoes
+          arq_alterado;
+          msg_commit;
+          echo "$Tag: $msg_final - Arq: $arquivo - In: $branch_atual"
+          MSG="$Tag: $msg_final - Arq: $arquivo - In: $branch_atual";
+fi;     
+
+# Encerrando a mensagem final e realizando o commit 
 read -p "A mensagem esta correta, podemos salvar ? [s\n] -> " resp_msg_final; 
 case $resp_msg_final in 
      s|S|sim|SIM|sIM|Sim|siM) git cm "$MSG";;
      *) echo "Chame o semantic novamente ...";;
-esac
-git status
+esac;
+git status;
